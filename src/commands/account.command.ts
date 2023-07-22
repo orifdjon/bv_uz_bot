8fb6f6df3542904b9cca9772febaae8af2e9bfb6
Акбар <xojayev97@list.ru>
2023-07-22 19:22:57 +0500

    добавил структуру первого уровня

--- src/commands/account.command.ts
+++ src/commands/account.command.ts
@@ -1,10 +1,27 @@
 import { Command } from './command.class'
+import { Markup } from 'telegraf'
 
 export class AccountCommand extends Command {
-
   handle (): void {
     this.bot.command('account', (ctx) => {
-      void ctx.reply('Ваш аккаунт')
+      void ctx.reply('Больше инфо', Markup.inlineKeyboard([
+        Markup.button.callback('Добавьте кое-что еще о себе', 'more_info'),
+        Markup.button.callback('Cashback', 'cashback')
+      ]))
+    })
+
+    this.bot.action('more_info', (ctx) => {
+      ctx.session.courseLike = true
+    })
+
+    this.bot.action('cashback', (ctx) => {
+      ctx.session.courseLike = true
+      void ctx.editMessageText('У вас столько-то кешбэка')
+    })
+
+    this.bot.action('cashback', (ctx) => {
+      ctx.session.courseLike = true
+      void ctx.editMessageText('Изменение ваших данных')
     })
   }
 }
