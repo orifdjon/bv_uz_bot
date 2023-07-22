f07a7d713d3e62470444e5acf6ee5bb54e6fd3d9
Акбар <xojayev97@list.ru>
2023-07-22 14:51:33 +0500

    удалил лишний код

--- src/commands/account.command.ts
+++ src/commands/account.command.ts
@@ -1,12 +1,6 @@
 import { Command } from './command.class'
-import { type IBotContext } from '../context/context.interface'
-import { type Telegraf } from 'telegraf'
 
 export class AccountCommand extends Command {
-  constructor (bot: Telegraf<IBotContext>) {
-    console.log('asdasd')
-    super(bot)
-  }
 
   handle (): void {
     this.bot.command('account', (ctx) => {
--- src/commands/feedback.command.ts
+++ src/commands/feedback.command.ts
@@ -1,12 +1,6 @@
 import { Command } from './command.class'
-import { type IBotContext } from '../context/context.interface'
-import { type Telegraf } from 'telegraf'
 
 export class FeedbackCommand extends Command {
-  constructor (bot: Telegraf<IBotContext>) {
-    console.log('отзыв')
-    super(bot)
-  }
 
   handle (): void {
     this.bot.command('feedback', (ctx) => {
